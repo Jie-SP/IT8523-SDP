168871783471a7765ef4706aed30952a0e96dac8
Jie-SP <167431358+Jie-SP@users.noreply.github.com>
2024-06-06 14:58:24 +0000

    update for CA1

--- BACKEND/app.py
+++ BACKEND/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 #from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from models_crud import initialize_database
+from models_crud import initialize_database, create_task
 # from auth import auth_bp  # Import the Blueprint
 from config import Config, DevelopmentConfig, ProductionConfig
 from flask_cors import CORS
@@ -30,6 +30,24 @@ with app.app_context():
 def hello():
     return render_template("index.html")
     # return "Hello, World!"
+
+# create newe task
+@app.route("/tasks", methods=['POST'])
+def create_new_task ():
+    data = request.get_json()
+    name = data.get('name')
+    desc = data.get('description')
+    points = data.get('points')
+    image_url = data.get('image_url')
+
+    try:
+        task = create_task(name, desc, points, image_url)
+        return jsonify({"message": "Task created successfully", "task": task}), 201
+    
+    except Exception as e:
+        return jsonify({"message": "Task creation failed", "details": str(e)}), 400
+
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="127.0.0.1")
--- BACKEND/models_crud.py
+++ BACKEND/models_crud.py
@@ -2,19 +2,59 @@ from extensions import db
 from sqlalchemy import text, exc
  
 def create_user_tables():
-    user_table_sql = text("""
+    user_task_sql = text("""
         CREATE TABLE IF NOT EXISTS Task (
-        `taskId` INT AUTO_INCREMENT PRIMARY KEY,
-        `name` VARCHAR(255),
-        `description` TEXT,
-        `points` INT,
-        `image_url` TEXT
+            taskId INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            description TEXT,
+            points INT,
+            image_url TEXT
         )ENGINE=InnoDB;
     """)
 
+    task_progress_sql = text("""
+        CREATE TABLE IF NOT EXISTS TaskProgress (
+            progressId INT AUTO_INCREMENT PRIMARY KEY,
+            userId INT NOT NULL,
+            taskId INT NOT NULL,
+            completion_date TIMESTAMP,
+            notes TEXT,
+            FOREIGN KEY (userId) REFERENCES user(userId),
+            FOREIGN KEY (taskId) REFERENCES Task(taskId)
+        )ENGINE=INNODB;
+    """)
+
+    user_table_sql = text("""
+        CREATE TABLE IF NOT EXISTS user (
+            userId INT AUTO_INCREMENT PRIMARY KEY,
+            username VARCHAR(50),
+            email VARCHAR(100)
+        )ENGINE=INNODB;
+    """)
+
     with db.engine.begin() as connection:
+        connection.execute(user_task_sql)
+        connection.execute(task_progress_sql)
         connection.execute(user_table_sql)
  
 def initialize_database():
     """Create user tables if they don't exist before the first request."""
     create_user_tables()
+
+
+# create task
+def create_task(name, description, points, image_url):
+    try:
+        create_task_sql = text ("""
+            INSERT INTO Task (name, description, points, image_url) 
+            VALUES (:name, :description, :points, :image_url)
+        """)
+
+        db.session.execute(create_task_sql, {'name': name, 'description': description, 'points': points, 'image_url': image_url})
+        task = db.session.execute(text('SELECT LAST_INSERT_ID()')).fetchone()[0]
+        db.session.commit()
+
+        return task
+    except Exception as e:
+        db.session.rollback()
+        raise e
